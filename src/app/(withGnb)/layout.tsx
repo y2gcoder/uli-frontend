c18b6e77c085bc6d21334f26c5a1d41cbe74079a
y2gcoder <y2gcoder@gmail.com>
2025-05-28 01:44:20 +0900

    RootLayout 으로 헤더와 푸터 이동

--- src/app/(withGnb)/layout.tsx
+++ src/app/(withGnb)/layout.tsx
@@ -1,9 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header>GNB</header>
       {children}
-      <footer>FOOTER</footer>
     </>
   );
 }
